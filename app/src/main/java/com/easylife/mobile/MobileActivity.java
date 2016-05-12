a7d291d1d2c5e7291e13bf8dc49c8d186e8d5e67
Kevin <gubaijing2004@163.com>
2016-05-12 23:11:36 +0800

    update

--- app/src/main/java/com/easylife/mobile/MobileActivity.java
+++ app/src/main/java/com/easylife/mobile/MobileActivity.java
@@ -1,15 +1,25 @@
 package com.easylife.mobile;
 
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.easylife.R;
+import com.easylife.base.BaseActivity;
+import com.easylife.custom.TopBar;
 
-public class MobileActivity extends AppCompatActivity {
+public class MobileActivity extends BaseActivity {
+    private TopBar topBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile);
+        topBar = (TopBar) findViewById(R.id.topbar);
+        topBar.setCenterText("手机充值");
+
+        textView = (TextView) findViewById(R.id.mobile_txt);
+        textView.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 }
